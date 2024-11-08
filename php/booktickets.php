bdfa80c4980cf2d400d6c87b2d5aa8042dbcaca9
arpitkumar0007 <arpitk@iitbhilai.ac.in>
2024-11-08 19:18:22 +0530

    added some more functionalities in booktickets.php

--- php/booktickets.php
+++ php/booktickets.php
@@ -53,4 +53,181 @@ $search_performed = false;
 $booking_success = false;
 $generated_pnr = "";
 $seat = "";
+// Function to generate PNR
+function generatePNR() {
+    return mt_rand(1000000000, 9999999999);
+}
+// Function to generate seat number
+function generateSeatNumber($class, $berth_preference) {
+    // Initialize variables
+    $coach_number = '';
+    $berth_code = '';
+    $max_seats = 0;
+
+    switch ($class) {
+        case '1A':
+            $coach_number = 'HA' . mt_rand(1, 3);
+            $max_seats = 18;
+            break;
+        case '2A':
+            $coach_number = 'A' . mt_rand(1, 5);
+            $max_seats = 46;
+            break;
+        case '3A':
+            $coach_number = 'B' . mt_rand(1, 7);
+            $max_seats = 64;
+            break;
+        case 'SL':
+            $coach_number = 'S' . mt_rand(1, 10);
+            $max_seats = 72;
+            break;
+        default:
+            $coach_number = 'S1';
+            $max_seats = 72;
+    }
+
+    switch ($berth_preference) {
+        case 'Lower':
+            $berth_code = 'LB';
+            $seat_number = generateNumberForBerthType($max_seats, 1);
+            break;
+        case 'Middle':
+            $berth_code = 'MB';
+            $seat_number = generateNumberForBerthType($max_seats, 2);
+            break;
+        case 'Upper':
+            $berth_code = 'UB';
+            $seat_number = generateNumberForBerthType($max_seats, 3);
+            break;
+        case 'Side Lower':
+            $berth_code = 'SL';
+            $seat_number = generateSideNumber($max_seats, true);
+            break;
+        case 'Side Upper':
+            $berth_code = 'SU';
+            $seat_number = generateSideNumber($max_seats, false);
+            break;
+        default:
+            $berth_code = 'LB';
+            $seat_number = generateNumberForBerthType($max_seats, 1);
+    }
+    
+    return [
+        'coach' => $coach_number,
+        'berth_type' => $berth_code,
+        'seat_number' => $seat_number,
+        'full_seat' => $coach_number . '-' . $berth_code . $seat_number
+    ];
+}
+
+function generateNumberForBerthType($max_seats, $remainder) {
+    $possible_numbers = range($remainder, $max_seats - 2, 3);
+    return $possible_numbers[array_rand($possible_numbers)];
+}
+
+function generateSideNumber($max_seats, $is_lower) {
+    if ($is_lower) {
+        $possible_numbers = range(7, $max_seats, 8);
+    } else {
+        $possible_numbers = range(8, $max_seats, 8);
+    }
+    return $possible_numbers[array_rand($possible_numbers)];
+}
+
+if (isset($_POST['search_trains'])) {
+    $from_station = $conn->real_escape_string($_POST['from_station'] ?? '');
+    $to_station = $conn->real_escape_string($_POST['to_station'] ?? '');
+    $journey_date = $conn->real_escape_string($_POST['journey_date'] ?? '');
+    $class = $conn->real_escape_string($_POST['class'] ?? '');
+    $day_of_week = date('l', strtotime($journey_date));
+
+    $query = "SELECT t.*, 
+    CASE 
+        WHEN '$class' = '1A' THEN base_ac1_fare
+        WHEN '$class' = '2A' THEN base_ac2_fare
+        WHEN '$class' = '3A' THEN base_ac3_fare
+        WHEN '$class' = 'SL' THEN base_sleeper_fare
+        ELSE 0
+    END as selected_fare,
+    TIMEDIFF(arrival_time, departure_time) as duration_calc
+    FROM trains t
+    WHERE from_station LIKE '%$from_station%' 
+    AND to_station LIKE '%$to_station%' 
+    AND FIND_IN_SET('$day_of_week', runs_on) > 0 
+    AND train_status != 'CANCELLED'";
+
+    $result = $conn->query($query);
+    if ($result) {
+        $search_results = $result->fetch_all(MYSQLI_ASSOC);
+        $result->free_result();
+    }
+    $search_performed = true;
+}
+
+if (isset($_POST['book_ticket'])) {
+    $pnr = generatePNR();
+    $seat_details = generateSeatNumber($_POST['class'], $_POST['berth_preference']);
+    
+    $class_mapping = [
+        'ac1' => '1A',
+        'ac2' => '2A',
+        'ac3' => '3A',
+        'sleeper' => 'SL'
+    ];
+    
+    $class_value = isset($class_mapping[$_POST['class']]) ? 
+                   $class_mapping[$_POST['class']] : 
+                   $_POST['class'];
+    
+    $insert_query = "INSERT INTO booking (
+        pnr_number, train_number, passenger_name, gender, age,
+        document_type, document_number, berth_preference, allocated_seat,
+        booking_date, journey_date, from_station, to_station, class, fare
+    ) VALUES (
+        ?, ?, ?, ?, ?,
+        ?, ?, ?, ?,
+        CURDATE(), ?, ?, ?, ?, ?
+    )";
+    
+    try {
+        $stmt = $conn->prepare($insert_query);
+        if (!$stmt) {
+            throw new Exception("Prepare failed: " . $conn->error);
+        }
+        
+        if (!$stmt->bind_param("ssssississsssd",
+    $pnr,
+    $_POST['train_number'],
+    $_POST['passenger_name'],
+    $_POST['gender'],
+    $_POST['age'],
+    $_POST['document_type'],
+    $_POST['document_number'],
+    $_POST['berth_preference'],
+    $seat_details['full_seat'],  
+    $_POST['journey_date'],
+    $_POST['from_station'],
+    $_POST['to_station'],
+    $_POST['class'],
+    $_POST['fare']
+)) {
+            throw new Exception("Binding parameters failed: " . $stmt->error);
+        }
+        
+        if (!$stmt->execute()) {
+            throw new Exception("Execute failed: " . $stmt->error);
+        }
+        
+        $booking_success = true;
+        $generated_pnr = $pnr;
+        $generated_seat = $seat_details;
+        
+    } catch (Exception $e) {
+        die("Booking failed: " . $e->getMessage());
+    } finally {
+        if (isset($stmt)) {
+            $stmt->close();
+        }
+    }
+}
 ?>
