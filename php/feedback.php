35a9868b7b5837183848125c8fc696b2c7734967
keshavmishra21 <keshavm@iitbhilai.ac.in>
2024-11-09 23:54:38 +0530

    Added the basic html of feedback.php

---
+++ php/feedback.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Feedback - RailwayYatri</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
+</head>
+<body>
+</body>
+</html>
