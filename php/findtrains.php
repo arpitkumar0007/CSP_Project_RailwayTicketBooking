e3b8d1f30f892fd7147688b2e3d72e8188b1ec8d
arpitkumar0007 <arpitk@iitbhilai.ac.in>
2024-11-09 23:48:58 +0530

    added the stylesheet

--- php/findtrains.php
+++ php/findtrains.php
@@ -251,7 +251,7 @@ if (isset($_POST['book_ticket'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RailwayYatri - Modern Booking Experience</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    
+    <link rel="stylesheet" href="/css/findtrains.css">
 </head>
 <body>
     <div class="container">
